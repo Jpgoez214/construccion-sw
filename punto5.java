00b3b06f21a12b03461d6804feb41645807feffb
Jpgoez214 <161983695+Jpgoez214@users.noreply.github.com>
2024-03-02 19:07:15 -0500

    Update punto5.java

--- punto5.java
+++ punto5.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author 57301
  */
-public class p {
+public class punto5 {
 
     public static void main(String[] args) {
          int[][] matrix = {{0,1,2,3},{4,5,6,7},{8,9,0,1},{2,3,4,5}};
